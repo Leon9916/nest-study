64da90e402052fc861ee941e9be29b8465abc5a9
liao <ao.li@wanbexpress.com>
2025-02-25 18:30:45 +0800

    基于 Redis 实现关注关系

---
+++ following/src/redis/redis.service.ts
@@ -0,0 +1,28 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { RedisClientType } from 'redis';
+
+@Injectable()
+export class RedisService {
+  @Inject('REDIS_CLIENT')
+  private redisClient: RedisClientType;
+
+  async sAdd(key: string, ...members: string[]) {
+    return this.redisClient.sAdd(key, members);
+  }
+
+  async sInterStore(newSetKey: string, set1: string, set2: string) {
+    return this.redisClient.sInterStore(newSetKey, [set1, set2]);
+  }
+
+  async sIsMember(key: string, member: string) {
+    return this.redisClient.sIsMember(key, member);
+  }
+
+  async sMember(key: string) {
+    return this.redisClient.sMembers(key);
+  }
+
+  async exists(key: string) {
+    return (await this.redisClient.exists(key)) > 0;
+  }
+}
