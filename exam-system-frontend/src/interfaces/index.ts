e9e4fbb8c27136f7e0550992efe2b64755b41105
liao <ao.li@wanbexpress.com>
2025-04-08 17:25:46 +0800

    feature(): 1: 考试系统：登录、注册页面 2: 考试系统：修改密码、试卷列表页面 3: 考试系统：新增试卷、回收站

---
+++ exam-system-frontend/src/interfaces/index.ts
@@ -0,0 +1,106 @@
+import axios, { AxiosResponse, InternalAxiosRequestConfig } from "axios";
+import { RegisterUser } from "../pages/Register";
+import { UpdatePassword } from "../pages/UpdatePassword";
+import message from "antd/es/message";
+import { ExamAdd } from "../pagesListAddModal";
+
+const userServiceInstance = axios.create({
+  baseURL: "http://localhost:3001/",
+  timeout: 3000,
+});
+
+export async function login(username: string, password: string) {
+  return await userServiceInstance.post("/login", {
+    username,
+    password,
+  });
+}
+
+export async function registerCaptcha(email: string) {
+  return await userServiceInstance.get("/register-captcha", {
+    params: {
+      address: email,
+    },
+  });
+}
+
+export async function register(registerUser: RegisterUser) {
+  return await userServiceInstance.post("/register", registerUser);
+}
+
+export async function updatePasswordCaptcha(email: string) {
+  return await userServiceInstance.get("/update_password/captcha", {
+    params: {
+      address: email,
+    },
+  });
+}
+
+export async function updatePassword(data: UpdatePassword) {
+  return await userServiceInstance.post("/update_password", data);
+}
+
+const examServiceInstance = axios.create({
+  baseURL: "http://localhost:3002/",
+  timeout: 3000,
+});
+
+const requestInterceptor = function (config: InternalAxiosRequestConfig) {
+  const accessToken = localStorage.getItem("token");
+
+  if (accessToken) {
+    config.headers.authorization = "Bearer " + accessToken;
+  }
+  return config;
+};
+
+examServiceInstance.interceptors.request.use(requestInterceptor);
+
+const responseIntercepor = (response: AxiosResponse) => {
+  const newToken = response.headers["token"];
+  if (newToken) {
+    localStorage.setItem("token", newToken);
+  }
+  return response;
+};
+
+const responseErrorIntercepor = async (error: any) => {
+  if (!error.response) {
+    return Promise.reject(error);
+  }
+  let { data } = error.response;
+  if (data.statusCode === 401) {
+    message.error(data.message);
+
+    setTimeout(() => {
+      window.location.href = "/login";
+    }, 1500);
+  } else {
+    return Promise.reject(error);
+  }
+};
+
+examServiceInstance.interceptors.response.use(
+  responseIntercepor,
+  responseErrorIntercepor
+);
+
+export async function examList() {
+  return await examServiceInstance.get("/list");
+}
+
+export async function examAdd(values: ExamAdd) {
+  return await examServiceInstance.post("/add", values);
+}
+
+export async function examPublish(id: number) {
+  return await examServiceInstance.get("/publish/" + id);
+}
+
+export async function examUnpublish(id: number) {
+  return await examServiceInstance.get("/unpublish/" + id);
+}
+
+export async function examDelete(id: number) {
+  return await examServiceInstance.delete("/delete/" + id);
+}
---
+++ exam-system-frontend/src/main.tsx
@@ -0,0 +1,32 @@
+import ReactDOM from "react-dom/client";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { Register } from "./pages/Register";
+import { Login } from "./pages/Login";
+import { UpdatePassword } from "./pages/UpdatePassword";
+import { ExamList } from "./pages/ExamList";
+
+const routes = [
+  {
+    path: "/",
+    element: <ExamList />,
+  },
+  {
+    path: "login",
+    element: <Login />,
+  },
+  {
+    path: "register",
+    element: <Register />,
+  },
+  {
+    path: "update_password",
+    element: <UpdatePassword />,
+  },
+];
+const router = createBrowserRouter(routes);
+
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+
+root.render(<RouterProvider router={router} />);
